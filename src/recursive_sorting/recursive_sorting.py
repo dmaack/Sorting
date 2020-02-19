b6e7b985fe041d90724025253dacd1daff15e6f2
dominique maack <dominiquemaack@gmail.com>
2020-02-18 17:32:19 -0700

    refactored merge function with my practice code file and is now working

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -14,25 +14,23 @@ def merge( arrA, arrB ):
 
     while arrA_index < len(arrA) and arrB_index < len(arrB): 
         if arrA[arrA_index] > arrB[arrB_index]:
-            merged_arr[merged_index] ==  arrB[arrB_index]
-            merged_index + 1
-            arrB_index + 1
-        elif arrA[arrA_index] < arrB[arrB_index]:
-            merged_arr[merged_index] ==  arrA[arrA_index]
-            merged_index + 1
-            arrA_index + 1
+            merged_arr[merged_index] =  arrB[arrB_index]
+            merged_index = merged_index + 1
+            arrB_index = arrB_index + 1
+        else:
+            merged_arr[merged_index] =  arrA[arrA_index]
+            merged_index = merged_index + 1
+            arrA_index = arrA_index + 1
     
-    for a in arrA:
-        arrA_index = a
-        if arrA_index < len(arrA):
-            merged_arr[merged_index] == arrA[a]
-            a + 1
+    while arrA_index < len(arrA):
+        merged_arr[merged_index] = arrA[arrA_index]
+        merged_index = merged_index + 1
+        arrA_index = arrA_index + 1
     
-    for b in arrB:
-        arrB_index = b
-        if arrB_index < len(arrB):
-            merged_arr[merged_index] == arrB[b]
-            b + 1
+    while arrB_index < len(arrB):
+        merged_arr[merged_index] = arrB[arrB_index]
+        merged_index = merged_index + 1
+        arrB_index = arrB_index + 1
 
     return merged_arr
 
