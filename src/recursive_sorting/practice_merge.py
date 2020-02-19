f4820bae947929e3310d68e53703d898fce89c4b
dominique maack <dominiquemaack@gmail.com>
2020-02-18 17:29:55 -0700

    refactored practice merge code from sytax errors and added missing logic -- now working

--- src/recursive_sorting/practice_merge.py
+++ src/recursive_sorting/practice_merge.py
@@ -1,5 +1,5 @@
-arr1 = [2, 3, 6, 7]
-arr2 = [1, 4, 5]
+arrA = [3, 7, 10, 11, 12 , 13]
+arrB = [2, 6, 8]
 
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
@@ -11,26 +11,24 @@ def merge( arrA, arrB ):
 
     while arrA_index < len(arrA) and arrB_index < len(arrB): 
         if arrA[arrA_index] > arrB[arrB_index]:
-            merged_arr[merged_index] ==  arrB[arrB_index]
-            merged_index + 1
-            arrB_index + 1
-        elif arrA[arrA_index] < arrB[arrB_index]:
-            merged_arr[merged_index] ==  arrA[arrA_index]
-            merged_index + 1
-            arrA_index + 1
+            merged_arr[merged_index] =  arrB[arrB_index]
+            merged_index = merged_index + 1
+            arrB_index = arrB_index + 1
+        else:
+            merged_arr[merged_index] =  arrA[arrA_index]
+            merged_index = merged_index + 1
+            arrA_index = arrA_index + 1
     
-    for a in arrA:
-        arrA_index = a
-        if arrA_index < len(arrA):
-            merged_arr[merged_index] == arrA[a]
-            a + 1
+    while arrA_index < len(arrA):
+        merged_arr[merged_index] = arrA[arrA_index]
+        merged_index = merged_index + 1
+        arrA_index = arrA_index + 1
     
-    for b in arrB:
-        arrB_index = b
-        if arrB_index < len(arrB):
-            merged_arr[merged_index] == arrB[b]
-            b + 1
+    while arrB_index < len(arrB):
+        merged_arr[merged_index] = arrB[arrB_index]
+        merged_index = merged_index + 1
+        arrB_index = arrB_index + 1
 
     return merged_arr
 
-print(merge(arr1, arr2))
+print(merge(arrA, arrB))
